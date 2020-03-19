0754cdfc619aa5c3f8d02d4fb5d26cbcf116d5cd
sodwater <794812941@qq.com>
2020-03-19 10:24:12 +0800

    项目经理第一次提交

---
+++ src/main/java/com/it/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.it.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 作者：钟伟
+ * 时间：2020/3/19 9:09
+ */
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("info")
+    public String info(){
+        return "hello1";
+    }
+}
